8f716b36bd7e70e99eac38d8ca8521eaeb2ad3da
shibin <shibiny6@gmail.com>
2022-06-13 15:29:51 +0800

    feat: 🎸 test

---
+++ core/core/bin/index.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+const importLocal = require('import-local')
+if(importLocal(__filename)){
+    console.log('正在使用本地版本')
+}else{
+    require('../lib/core')(process.argv.slice(2))
+}
