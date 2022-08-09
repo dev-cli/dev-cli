8e855ee33205df7637406ffb3306344bf6062b50
shibin <shibin@shibindeMac-mini.local>
2022-08-10 00:21:51 +0800

    feat: 🎸 add init utils

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -2,26 +2,52 @@
 
 module.exports = exec;
 const Package = require('@dev-cli/package')
+const log = require('@dev-cli/log')
+const path = require('path')
 
 const SETTINGS = {
-    init: '@dev-cli/init'
+    init: '@dev-cli/utils'
 }
-
-function exec(a, b) {
+const CACHE_DIR = 'dependencies'
+async function exec() {
     // const opts = this.opts()
     const cmdObj = arguments[arguments.length - 1]
     let targetPath = process.env.CLI_TARGET_PATH
     const packageName = SETTINGS[cmdObj.name()]
     console.log(packageName)
-    if (!targetPath) {
-        targetPath = ''
-    }
+    const homePath = process.env.CLI_HOME_PATH
+    let pkg
+    let storeDir = ''
     const packageVersion = 'latest'
-    const pkg = new Package({
-        targetPath,
-        packageName,
-        packageVersion
-    })
-    console.log(pkg.getRootFilePath())
-    console.log(pkg)
+    if (!targetPath) {
+
+        targetPath = path.resolve(homePath, CACHE_DIR)
+        storeDir = path.resolve(targetPath, 'node_modules')
+        pkg = new Package({
+            targetPath,
+            packageName,
+            packageVersion,
+            storeDir
+        })
+        if (pkg.exits()) {
+            // 更新package
+
+        } else {
+          await  pkg.install()
+        }
+
+
+    } else {
+        pkg = new Package({
+            targetPath,
+            packageName,
+            packageVersion
+        })
+    
+    }
+    const rootFile = pkg.getRootFilePath()
+    log.verbose('rootFile ', rootFile)
+    require(rootFile).apply(this, arguments)
+    log.verbose('pkg', pkg)
+
 }
