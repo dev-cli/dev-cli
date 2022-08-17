7ab2c5b9cbbae5f96845479e3c8f84862eb9fb58
shibin <shibin@shibindeMac-mini.local>
2022-08-17 23:27:02 +0800

    feat: 🎸 init localpath

--- command/init/lib/index.js
+++ command/init/lib/index.js
@@ -1,14 +1,34 @@
 const Command = require('@dev-cli/command')
+const log = require('@dev-cli/log')
+const fs = require('fs')
 
 class InitCommand extends Command {
     init() {
         this.projectName = this._argv[0] || ''
         this.force = !!this._argv[1].force
-        console.log(this.projectName)
-        console.log(this.force)
+        console.log('init')
+        log.verbose('project name', this.projectName)
     }
     exec() {
+        try {
+            this.prepare()
+        } catch (e) {
+            log.error(e.message)
+        }
+    }
+    prepare() {
+        // 判断当前目录是否为空
+        if (!this.isDirIsEmpty()) {
 
+        } else {
+
+        }
+    }
+    isDirIsEmpty() {
+        const localPath = process.cwd()
+        console.log(localPath)
+        const fileList = fs.readdirSync(localPath).filter(file => !(file.startsWith('.') && ['node_modules'].indexOf(file)))
+        return fileList.length === 0
     }
 }
 
